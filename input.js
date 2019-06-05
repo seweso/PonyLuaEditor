125bdfa7dd11cd7bc3b203dad78cf520a11fd64d
Alexander Hausmann <alexanderhausmann@hotmail.de>
2019-06-06 00:33:35 +0200

    do not allow inputs with same labels, automatically preselect options for the add select

--- input.js
+++ input.js
@@ -6,14 +6,16 @@ var INPUT = ((global, $)=>{
 
     let dom
     let dom_bools
+    let dom_bools_add
     let dom_numbers
+    let dom_numbers_add
 
     function init(container){
         dom = $(container)
         dom.append('<div class="head">Inputs:</div>')
 
         dom_bools = $('<div class="bools"><div class="head"><span>Booleans:</span></div></div>')
-        let dom_bools_add = $('<div class="add"><select></select><button>+</button></div>')
+        dom_bools_add = $('<div class="add"><select></select><button>+</button></div>')
         for(let i = 1; i < 33; i++){
             dom_bools_add.find('select').append('<option value="'+i+'">'+i+'</option>')
         }
@@ -26,9 +28,9 @@ var INPUT = ((global, $)=>{
         })
         dom_bools.find('.head').append(dom_bools_add)
         dom.append(dom_bools)
-
+        
         dom_numbers = $('<div class="numbers"><div class="head"><span>Numbers:</span></div></div>')
-        let dom_numbers_add = $('<div class="add"><select></select><button>+</button></div>')
+        dom_numbers_add = $('<div class="add"><select></select><button>+</button></div>')
         for(let i = 1; i < 33; i++){
             dom_numbers_add.find('select').append('<option value="'+i+'">'+i+'</option>')
         }
@@ -55,6 +57,7 @@ var INPUT = ((global, $)=>{
             }
         }
 
+
         if(typeof store.numbers === 'object' && store.numbers !== null){
             for(let k of Object.keys(store.numbers)){
                 let n = parseInt(store.numbers[k])
@@ -71,6 +74,22 @@ var INPUT = ((global, $)=>{
                 addNewNumber(parseInt(k), store.numbers[k])
             }
         }
+    }
+
+    function refreshBoolsAddSelect(){
+        dom_bools.find('.bool').prop('selected', false)
+        let i = dom_bools.find('.bool:last-of-type label').html()
+        i = parseInt(i)
+        i = isNaN(i) ? 0 : i
+        dom_bools_add.find('option[value="' + (i+1) + '"]').prop('selected', true)
+    }
+
+    function refreshNumbersAddSelect(){
+        dom_numbers.find('.number').prop('selected', false)
+        let i = dom_numbers.find('.number:last-of-type label').html()
+        i = parseInt(i)
+        i = isNaN(i) ? 0 : i
+        dom_numbers_add.find('option[value="' + (i+1) + '"]').prop('selected', true)
     }
 
 
@@ -103,19 +122,28 @@ var INPUT = ((global, $)=>{
         if(typeof label !== 'number' || label.length === 0){
             return
         }
+        if(bools[label] !== undefined){
+            return
+        }
         bools[label] = val === true ? true : false
         let bool = addNew('bool', 'checkbox', label, (e)=>{
             bools[label] = $(e.target).prop('checked')
+            refreshBoolsAddSelect()
         }, (e)=>{
             bools[label] = false
             delete bools[label]
             $(e.target).parent().remove()
+            refreshBoolsAddSelect()
         }, val)
-        dom_bools.append(bool)  
+        dom_bools.append(bool)
+        refreshBoolsAddSelect()
     }
 
     function addNewNumber(label, val){
         if(typeof label !== 'number' || label.length === 0){
+            return
+        }
+        if(numbers[label] !== undefined){
             return
         }
         numbers[label] = typeof val === 'number' ? val : 0
@@ -128,12 +156,15 @@ var INPUT = ((global, $)=>{
                 return
             }
             numbers[label] = n
+            refreshNumbersAddSelect()
         }, (e)=>{
             numbers[label] = 0
             delete numbers[label]
             $(e.target).parent().remove()
+            refreshNumbersAddSelect()
         }, val)
-        dom_numbers.append(number)  
+        dom_numbers.append(number)
+        refreshNumbersAddSelect()
     }
 
     function addNew(type, inputType, label, changeCallback, removeCallback, val){
@@ -152,6 +183,7 @@ var INPUT = ((global, $)=>{
             removeCallback(e)
             saveToStorage()
         })
+        saveToStorage()
         return neww
     }
 
