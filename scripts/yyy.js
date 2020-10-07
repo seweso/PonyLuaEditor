4cf462c18da54ced0988594d603a74358767b0ac
Alexander Hausmann <alexander.hausmann@alpstein.local>
2020-10-07 20:21:54 +0200

    #218 remove help until being refactored

--- scripts/yyy.js
+++ scripts/yyy.js
@@ -19,58 +19,9 @@ YYY = (($)=>{
             }, 1000 * 60 * 10)
         }, 1000 * 60 * 30)
 
-        if(document.location.pathname.indexOf('beta') >= 0 || document.location.host === 'localhost'){
-            $('#beta').show()
-        }
 
-        if(!document.location.host || !document.location.href || document.location.href.indexOf('file') === 0){
-            $('#offline').show()
-            $('#download-offline').hide()
-            $('#share').hide()
-        }
-
-
-        $('#download-offline').on('click', ()=>{
-            REPORTER.report(REPORTER.REPORT_TYPE_IDS.downloadOffline)
-            UTIL.message('How to use the offline version:', '<ul><li>extract the zip folder</li><li>doubleclick "index.html"</li><li>This opens the offline version with your default browser</li><ul>')
-        })
-
-
-        /* help badge */
-        let firstHelpOpen = true
-
-        $('#help-badge, #help-menu-entry').on('click', ()=>{
-            REPORTER.report(REPORTER.REPORT_TYPE_IDS.openHelp)
-
-            if(firstHelpOpen){
-                firstHelpOpen = false
-                $('#help-youtube-video').html('<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/Z8cLxmVd07c" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
-            }
-            $('#help').fadeIn()
-            resizeYoutubeIframe()
-        })
-        $(window).on('resize', ()=>{
-            resizeYoutubeIframe()
-        })
-
-        function resizeYoutubeIframe(){            
-            $('#help-youtube-video iframe').css({
-                width: $('#help-youtube-video').width(),
-                height: $('#help-youtube-video').width() / (16/9)
-            })
-        }
-
-
-        $('#help-close').on('click', closeHelp)
-        $('#help').on('click', (evt)=>{
-            closeHelp()
-        })
-        $('#help-content').on('click', (evt)=>{
-            evt.stopPropagation()
-        })
-
-        function closeHelp(){
-            $('#help').fadeOut()            
+        if(!document.location.href || document.location.href.indexOf('file') >= 0 ||  document.location.href.indexOf('localhost') >= 0){
+            $('#share').attr('style', 'display: none!important')
         }
 
 
