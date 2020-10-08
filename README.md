9fbd114bcc7c5a89a404b9f0148b6906a20f83c6
Alexander Hausmann <alexander.hausmann@alpstein.local>
2020-10-08 16:19:20 +0200

    #195 use gulp to combine javascript files

--- README.md
+++ README.md
@@ -11,3 +11,48 @@ https://fengari.io/
 
 ## Favicon
 Generated with https://realfavicongenerator.net/
+
+
+## Build Tools
+
+### Gulp
+
+install gulp
+```
+npm install --global gulp-cli
+```
+
+setup gulp in project
+```
+npm install --save-dev gulp
+```
+
+
+create `gulpfile.js`
+
+```javascript
+function defaultTask(cb){
+    
+    cb()
+}
+
+exports.default = defaultTask
+```
+
+run gulp
+```
+gulp
+```
+
+
+
+
+### Gulp-Concat
+```
+npm install --save-dev gulp-concat
+```
+
+Source javascript files are inside src/scripts
+All of them are concatenated and written into scripts/all.js
+Any libraries should be placed directly into scripts/lib/
+You must register any new script inside gulpfile.js
---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+ 
+gulp.task('default', function() {
+    let prefix = './src/scripts/'
+    let suffix = '.js'
+    let order = [ 'loader',
+        /* scripts without LOADER */
+        'util','translate','reporter','http_localhost','paint','map','luaparse','luamin','luaminy','luamax','minmax',
+
+        /* scripts requiring LOADER */
+        'giveaway','lua_emulator','stormworks_lua_api','storage','share','ui','ui_helpers','ui_builder','lua_console',
+        'examples','editor','autocompletion','documentation','documentation_definition_client','documentation_definition_server',
+        'yyy','engine','input','output','property','canvas'
+        ]
+
+    for(let i in order){
+        order[i] = prefix + order[i] + suffix
+    }
+
+
+
+    return gulp.src(order)
+        .pipe(concat('all.js', {newLine: '\n;\n'}))
+        .pipe(gulp.dest('./scripts/'))
+})
