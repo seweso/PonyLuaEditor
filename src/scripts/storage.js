06a5782af11b69a5d8d7f8a2d4e64ca62358d222
Alexander Hausmann <alexanderhausmann@hotmail.de>
2020-10-25 20:48:46 +0100

    fix new share codes read

--- src/scripts/storage.js
+++ src/scripts/storage.js
@@ -169,8 +169,8 @@ STORAGE = (()=>{
     function setFromShare(key, confJSON){
 
         let parsedSettings = parseOrUndefined(confJSON.settings)
-        
-        if(parsedSettings.version){
+
+        if(parsedSettings && parsedSettings.version){
             if(parsedSettings.version === VERSION){
                 processStorage(parsedSettings)
             } else {
