cc852869a9ff864f37d679313d30b482f30a608f
Alexander Hausmann <alexander.hausmann@alpstein.local>
2020-11-20 13:26:48 +0100

    adjust eag

--- src/scripts/easteregg.js
+++ src/scripts/easteregg.js
@@ -3,7 +3,7 @@ EASTEREGG = (()=>{
 	let cont
 	let sound
 
-	let timesPlayed = 0
+	let played = false
 
 	$(window).on('load', init)
 
@@ -12,45 +12,62 @@ EASTEREGG = (()=>{
 			return
 		}
 
-		let style = '.easteregg_image {position: fixed; z-index: 999999999; top: 50%; left: 50%; height: 90vh; width: calc(( ( 388 / 690 ) ) * 90vh); transform: translateX(-50%) translateY(-50%); background: black;}'
+		let style = '.easteregg_image {position: fixed; z-index: 999999999; top: 50%; left: 50%; height: 90vh; width: calc(( ( 388 / 690 ) ) * 90vh); transform: translateX(-50%) translateY(-50%); background: black; border: 3px solid #b80a66; border-radius: 3px;}'
 			+ '.easteregg_image .easteregg_image_gif {position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 1; background: url("dev-docs/easteregg_image.gif"); background-size: cover;}'
 			+ '.easteregg_image .easteregg_image_border {position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 2; background: url("dev-docs/easteregg_border.png"); background-size: 100% 100%;}'
+			+ '.easteregg_image .easteregg_image_close {position: absolute; top: 15px; left: 50%; width: 50px; height: 50px; transform: translateX(-50%); display: flex; justify-content: center; align-items: center; z-index: 3; opacity: 0; transition: all 0.2s ease 0s; border-radius: 8px; color: #fff; background: #b80a66; font-size: 18px; line-height: 18px; cursor: pointer;}'
+			+ '.easteregg_image:hover .easteregg_image_close {opacity: 1}'
+			+ '.easteregg_image_close:hover {background: #db84b2}'
 
 		$(document.body).append( $('<style>').text(style) )
 
-		cont = $('<div class="easteregg_image" style="display: none"><div class="easteregg_image_gif"></div><div class="easteregg_image_border"></div></div>')
+		cont = $('<div class="easteregg_image" style="display: none"><div class="easteregg_image_gif"></div><div class="easteregg_image_border"></div><div class="easteregg_image_close"><span class="icon-cross"></span></div>')
 
 		$(document.body).append(cont)
 
-		sound = new Audio('dev-docs/easteregg_sound.mp3')
+		cont.find('.easteregg_image_close').on('click', stop)
+
+		sound = new Audio('dev-docs/easteregg_sound.mp3?v=qsd71h')
 		sound.preload = true
-		sound.volume = 1
 		sound.onended = ()=>{
-			setTimeout(playOnce, 300)
+			stop()
 		}
 
 		setTimeout(()=>{
-			$(window).on('click', start)
+			$('#start').on('click', ()=>{
+				if(Math.random() * 50 < 1){
+					start()
+				}
+			})
+			$('.docreate').on('click', ()=>{
+				start()
+			})
 		}, 1000)
 	}
 
 	function shouldShowEasterEgg(){
-		return false
+		let params = new URLSearchParams(document.location.search)
+		let now = new Date()
+		let month = now.getMonth() + 1
+		let day = now.getDate()
+		return params.has('easteregg') || (month == 11 && day >= 11) || (month == 4 && day >= 1 && day <= 2)
 	}
 
-	function start(){
-		cont.show()
-		playOnce()
-	}
-
-	function playOnce(){
-		if(timesPlayed < 5){
-			sound.play()
-			timesPlayed++
-		} else {
-			cont.hide()
-			sound.volume = 0
+	function start(evt){
+		if(played){
+			return
 		}
+		played = true
+		$('#start').off('click', start)
+		$('.docreate').off('click', start)
+		cont.show()
+		sound.volume = 0.1
+		sound.play()
+	}
+
+	function stop(){
+		cont.hide()
+		sound.volume = 0
 	}
 
 })()
