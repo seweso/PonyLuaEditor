e8e7817dd18d246ee3530454a6106a311f9103ca
Wouter <seweso@gmail.com>
2022-11-16 13:54:32 +0100

    Add signalr

---
+++ src/scripts/stormnet.js
@@ -0,0 +1,28 @@
+﻿"use strict";
+
+//const {signalR} = require("../../scripts/lib/signalr");
+
+console.log("from stormnet.js")
+
+var connection = new signalR
+    .HubConnectionBuilder()
+    .withAutomaticReconnect()
+    .configureLogging(signalR.LogLevel.Information)
+    .withUrl("/signalR")
+    .build();
+
+connection.start().then(function () {
+    console.log("connected");
+
+    connection.invoke("SendMessage", "from stormnet.js").catch(function (err) {
+        return console.error(err.toString());
+    });
+    
+}).catch(function (err) {
+    console.error(err.toString());
+});
+
+
+connection.on("ReceiveMessage", function ( message) {
+   console.log("ReceiveMessage " + message);
+});
