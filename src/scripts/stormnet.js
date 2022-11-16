c2dd233c56cd2f0218a0dc4ceadefda7e7ff9783
Wouter <seweso@gmail.com>
2022-11-16 19:22:23 +0100

    Write inputs from SignalR

--- src/scripts/stormnet.js
+++ src/scripts/stormnet.js
@@ -13,16 +13,22 @@ var connection = new signalR
 
 connection.start().then(function () {
     console.log("connected");
-
-    connection.invoke("SendMessage", "from stormnet.js").catch(function (err) {
-        return console.error(err.toString());
-    });
-    
 }).catch(function (err) {
     console.error(err.toString());
 });
 
-
 connection.on("ReceiveMessage", function ( message) {
    console.log("ReceiveMessage " + message);
+});
+
+connection.on("GetDouble", function ( i, d) {
+    INPUT.setNumber(i+1, d, {
+        val:  d,
+        userLabel: "StormNet-" + (i+1),
+        slidercheck: true,
+        slidermin: -10000000,
+        slidermax: 10000000,
+        sliderstep: 0.0000000000001,
+        oscilatecheck: false
+    })
 });
