30aeb58a06ce6651e4ff3fa8655df68fc41bf053
Wouter <seweso@gmail.com>
2022-11-17 20:35:13 +0100

    Add tokens and signalr url

--- src/scripts/stormnet.js
+++ src/scripts/stormnet.js
@@ -2,16 +2,26 @@
 
 //const {signalR} = require("../../scripts/lib/signalr");
 
-console.log("from stormnet.js")
+let params = new URLSearchParams(document.location.search);
+let SignalR_Url = params.get('SignalR_Url');
+let SignalR_Token = params.get('SignalR_Token');
+
+console.log("from stormnet.js: " + SignalR_Url)
 
 var connection = new signalR
     .HubConnectionBuilder()
     .withAutomaticReconnect()
     .configureLogging(signalR.LogLevel.Information)
-    .withUrl("/signalR")
+    .withUrl(SignalR_Url)
     .build();
 
-connection.start();
+connection.start().then(async function () {
+    await connection.invoke("SetToken", SignalR_Token);
+})
+
+connection.onreconnected(async function () {
+    await connection.invoke("SetToken", SignalR_Token);
+})
 
 connection.on("GetDouble", function ( i, d) {
     INPUT.setNumber(i+1, d, {
